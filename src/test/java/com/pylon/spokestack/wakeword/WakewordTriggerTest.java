66bf949b934b15eb53c6d6079a96911b7131e10e
Brent M. Spell <bspell1@gmail.com>
2018-11-27 14:31:16 -0500

    flesh out behavioral tests

--- src/test/java/com/pylon/spokestack/wakeword/WakewordTriggerTest.java
+++ src/test/java/com/pylon/spokestack/wakeword/WakewordTriggerTest.java
@@ -18,16 +18,15 @@ import com.pylon.spokestack.SpeechContext;
 import com.pylon.spokestack.SpeechProcessor;
 import com.pylon.spokestack.tensorflow.TensorflowModel;
 
-public class WakewordTriggerTest implements OnSpeechEventListener {
-    private SpeechContext.Event event;
+public class WakewordTriggerTest {
 
     @Test
     public void testConstruction() throws Exception {
         final SpeechConfig config = new SpeechConfig();
 
         final TensorflowModel.Loader loader = spy(TensorflowModel.Loader.class);
-        final TensorflowModel filterModel = mock(TestModel.class);
-        final TensorflowModel detectModel = mock(TestModel.class);
+        final TestModel filterModel = mock(TestModel.class);
+        final TestModel detectModel = mock(TestModel.class);
         doReturn(filterModel).doReturn(detectModel).when(loader).load();
 
         // default config
@@ -87,12 +86,313 @@ public class WakewordTriggerTest implements OnSpeechEventListener {
     }
 
     @Test
-    public void testProcessing() throws Exception {
-        SpeechConfig config = new SpeechConfig()
+    public void testDetInactiveVadInactive() throws Exception {
+        // verify that filtering/detection models are not
+        // run and activation doesn't occur when vad is inactive
+        TestEnv env = new TestEnv(testConfig());
+
+        env.vad.active = false;
+        env.process();
+
+        verify(env.vad).process(any(SpeechContext.class), any(ByteBuffer.class));
+        verify(env.filter, never()).run();
+        verify(env.detect, never()).run();
+        reset(env.vad);
+
+        assertEquals(null, env.event);
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testDetInactiveVadActive() throws Exception {
+        // verify that filtering/detection is run when vad is active
+        // but that activation doesn't occur for the null class
+        TestEnv env = new TestEnv(testConfig());
+
+        env.vad.active = true;
+        env.process();
+
+        verify(env.filter, atLeast(1)).run();
+        verify(env.detect, atLeast(1)).run();
+
+        assertEquals(null, env.event);
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testDetInactiveVadDeactivate() throws Exception {
+        // verify that nothing happens when vad is deactivated
+        // for the null class
+        TestEnv env = new TestEnv(testConfig());
+
+        env.vad.active = true;
+        env.process();
+
+        env.vad.active = false;
+        env.process();
+
+        assertEquals(null, env.event);
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testDetActivate() throws Exception {
+        // verify a simple activation - single keyword phrase
+        TestEnv env = new TestEnv(testConfig());
+
+        env.vad.active = true;
+        env.detect.setOutputs(0, 1);
+        env.process();
+
+        env.detect.setOutputs(1, 0);
+        env.process();
+
+        assertEquals(SpeechContext.Event.ACTIVATE, env.event);
+        assertTrue(env.context.isActive());
+    }
+
+    @Test
+    public void testDetActiveVadDeactivate() throws Exception {
+        // verify deactivation on vad timeout after min activation length
+        TestEnv env = new TestEnv(testConfig());
+
+        env.vad.active = true;
+        env.detect.setOutputs(0, 1);
+        env.process();
+
+        env.detect.setOutputs(1, 0);
+        env.process();
+
+        env.process();
+        env.process();
+        env.process();
+
+        assertEquals(SpeechContext.Event.DEACTIVATE, env.event);
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testDetActiveMinDelay() throws Exception {
+        // verify no deactivation on vad timeout before min activation length
+        TestEnv env = new TestEnv(testConfig());
+
+        env.vad.active = true;
+        env.detect.setOutputs(0, 1);
+        env.process();
+
+        env.detect.setOutputs(1, 0);
+        env.process();
+
+        env.vad.active = false;
+        env.process();
+        env.process();
+
+        assertTrue(env.context.isActive());
+    }
+
+    @Test
+    public void testDetActiveTimeout() throws Exception {
+        // verify max activation timeout
+        TestEnv env = new TestEnv(testConfig());
+
+        env.vad.active = true;
+        env.detect.setOutputs(0, 1);
+        env.process();
+
+        env.detect.setOutputs(1, 0);
+        env.process();
+        env.process();
+
+        env.vad.active = false;
+        env.process();
+
+        assertEquals(SpeechContext.Event.DEACTIVATE, env.event);
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testDetManualVadDeactivate() throws Exception {
+        // verify manual activation followed by vad deactivation
+        TestEnv env = new TestEnv(testConfig());
+
+        env.context.setActive(true);
+        env.vad.active = true;
+        env.process();
+        env.process();
+
+        env.vad.active = false;
+        env.process();
+
+        assertEquals(SpeechContext.Event.DEACTIVATE, env.event);
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testDetManualMinDelay() throws Exception {
+        // verify manual activation remains with no vad activation/deactivation
+        TestEnv env = new TestEnv(testConfig());
+
+        env.context.setActive(true);
+        env.process();
+        env.process();
+        env.process();
+
+        assertTrue(env.context.isActive());
+    }
+
+    @Test
+    public void testDetManualTimeout() throws Exception {
+        // verify manual activation max activation timeout
+        TestEnv env = new TestEnv(testConfig());
+
+        env.context.setActive(true);
+        env.process();
+        env.process();
+        env.process();
+        env.process();
+
+        assertEquals(SpeechContext.Event.DEACTIVATE, env.event);
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testMultiWord() throws Exception {
+        // verify any word can trigger if no phrases configured
+        TestEnv env = new TestEnv(testConfig()
+            .put("wake-words", "up,dog"));
+
+        env.vad.active = true;
+
+        env.detect.setOutputs(0, 1, 0);
+        env.process();
+        env.detect.setOutputs(1, 0, 0);
+        env.process();
+
+        assertEquals(SpeechContext.Event.ACTIVATE, env.event);
+        assertTrue(env.context.isActive());
+        env.context.setActive(false);
+
+        env.detect.setOutputs(0, 0, 1);
+        env.process();
+        env.detect.setOutputs(1, 0, 0);
+        env.process();
+
+        assertEquals(SpeechContext.Event.ACTIVATE, env.event);
+        assertTrue(env.context.isActive());
+    }
+
+    @Test
+    public void testExactPhrase() throws Exception {
+        // verify only the specified phrase is matched if configured
+        TestEnv env = new TestEnv(testConfig()
+            .put("wake-words", "up,dog")
+            .put("wake-phrases", "up dog")
+            .put("wake-phrase-length", 30));
+
+        env.vad.active = true;
+
+        env.detect.setOutputs(0, 0, 1);
+        env.process();
+        env.detect.setOutputs(0, 1, 0);
+        env.process();
+        env.detect.setOutputs(1, 0, 0);
+        env.process();
+
+        assertFalse(env.context.isActive());
+
+        env.detect.setOutputs(0, 1, 0);
+        env.process();
+        env.detect.setOutputs(0, 0, 1);
+        env.process();
+        env.detect.setOutputs(1, 0, 0);
+        env.process();
+
+        assertEquals(SpeechContext.Event.ACTIVATE, env.event);
+        assertTrue(env.context.isActive());
+    }
+
+    @Test
+    public void testWord1PauseWord2() throws Exception {
+        // verify no activation if a vad deactivation occurs between keywords
+        TestEnv env = new TestEnv(testConfig()
+            .put("wake-words", "up,dog")
+            .put("wake-phrases", "up dog")
+            .put("wake-phrase-length", 40));
+
+        env.vad.active = true;
+        env.detect.setOutputs(0, 1, 0);
+        env.process();
+
+        env.vad.active = false;
+        env.process();
+
+        env.vad.active = true;
+        env.detect.setOutputs(0, 0, 1);
+        env.process();
+        env.detect.setOutputs(1, 0, 0);
+        env.process();
+
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testWord1WordsWord2() throws Exception {
+        // verify no activation if intermediate words fill the phrase buffer
+        TestEnv env = new TestEnv(testConfig()
+            .put("wake-words", "up,dog,yo")
+            .put("wake-phrases", "up dog")
+            .put("wake-phrase-length", 30));
+
+        env.vad.active = true;
+
+        env.detect.setOutputs(0, 1, 0, 0);
+        env.process();
+        env.detect.setOutputs(0, 0, 0, 1);
+        env.process();
+        env.detect.setOutputs(0, 0, 1, 0);
+        env.process();
+        env.detect.setOutputs(1, 0, 0, 0);
+        env.process();
+
+        assertFalse(env.context.isActive());
+    }
+
+    @Test
+    public void testSmoothing() throws Exception {
+        // verify that detector posteriors are smoothed with moving average
+        TestEnv env = new TestEnv(testConfig()
+            .put("wake-smooth-length", 20));
+
+        env.vad.active = true;
+
+        env.detect.setOutputs(1, 0);
+        env.process();
+        env.detect.setOutputs(0, 0.5f);
+        env.process();
+        env.detect.setOutputs(1, 0);
+        env.process();
+
+        assertFalse(env.context.isActive());
+
+        env.detect.setOutputs(0, 0.5f);
+        env.process();
+        env.detect.setOutputs(0, 0.5f);
+        env.process();
+        env.detect.setOutputs(1, 0);
+        env.process();
+
+        assertEquals(SpeechContext.Event.ACTIVATE, env.event);
+        assertTrue(env.context.isActive());
+    }
+
+    public SpeechConfig testConfig() {
+        return new SpeechConfig()
             .put("sample-rate", 16000)
             .put("frame-width", 10)
             .put("fft-hop-length", 10)
             .put("fft-window-size", 160)
+            .put("mel-frame-length", 40)
+            .put("mel-frame-width", 40)
             .put("wake-words", "hello")
             .put("wake-filter-path", "filter-path")
             .put("wake-detect-path", "detect-path")
@@ -100,109 +400,6 @@ public class WakewordTriggerTest implements OnSpeechEventListener {
             .put("wake-phrase-length", 20)
             .put("wake-active-min", 20)
             .put("wake-active-max", 30);
-
-        TensorflowModel.Loader loader = spy(TensorflowModel.Loader.class);
-        TensorflowModel filterModel = mock(TestModel.class);
-        TensorflowModel detectModel = mock(TestModel.class);
-        ByteBuffer frameBuffer = ByteBuffer.allocateDirect(160 * 4);
-        doReturn(ByteBuffer
-                .allocateDirect(81 * 4)
-                .order(ByteOrder.nativeOrder()))
-            .when(filterModel).inputs();
-        doReturn(ByteBuffer
-                .allocateDirect(40 * 4)
-                .order(ByteOrder.nativeOrder()))
-            .when(filterModel).outputs();
-        doReturn(ByteBuffer
-                .allocateDirect(40 * 40 * 4)
-                .order(ByteOrder.nativeOrder()))
-            .when(detectModel).inputs();
-        doReturn(ByteBuffer
-                .allocateDirect(2 * 4)
-                .order(ByteOrder.nativeOrder()))
-            .when(detectModel).outputs();
-        doCallRealMethod().when(filterModel).run();
-        doCallRealMethod().when(detectModel).run();
-        doReturn(filterModel).doReturn(detectModel).when(loader).load();
-
-        TestVad vad = spy(TestVad.class);
-        WakewordTrigger wake = new WakewordTrigger(config, loader, vad);
-
-        SpeechContext context = new SpeechContext();
-        context.addOnSpeechEventListener(this);
-
-        // inactive vad, inactive detector
-        vad.active = false;
-        wake.process(context, frameBuffer);
-        verify(vad).process(any(SpeechContext.class), any(ByteBuffer.class));
-        reset(vad);
-        verify(filterModel, never()).run();
-        verify(detectModel, never()).run();
-        assertEquals(null, this.event);
-        assertFalse(context.isActive());
-
-        // active vad, inactive detector
-        vad.active = true;
-        wake.process(context, frameBuffer);
-        verify(filterModel, atLeast(1)).run();
-        verify(detectModel, atLeast(1)).run();
-        assertEquals(null, this.event);
-        assertFalse(context.isActive());
-
-        // deactivated vad, inactive detector
-        vad.active = false;
-        wake.process(context, frameBuffer);
-        assertEquals(null, this.event);
-        assertFalse(context.isActive());
-
-        // detector activation
-        vad.active = true;
-
-        detectModel.outputs().rewind();
-        detectModel.outputs().putFloat(0);
-        detectModel.outputs().putFloat(1);
-        wake.process(context, frameBuffer);
-
-        detectModel.outputs().rewind();
-        detectModel.outputs().putFloat(1);
-        detectModel.outputs().putFloat(0);
-        wake.process(context, frameBuffer);
-
-        assertEquals(SpeechContext.Event.ACTIVATE, this.event);
-        assertTrue(context.isActive());
-
-        // deactivated vad, activated detector
-        wake.process(context, frameBuffer);
-
-        vad.active = false;
-        wake.process(context, frameBuffer);
-
-        assertEquals(SpeechContext.Event.DEACTIVATE, this.event);
-        assertFalse(context.isActive());
-
-        // timeout, activated detector
-        vad.active = true;
-
-        detectModel.outputs().rewind();
-        detectModel.outputs().putFloat(0);
-        detectModel.outputs().putFloat(1);
-        wake.process(context, frameBuffer);
-
-        detectModel.outputs().rewind();
-        detectModel.outputs().putFloat(1);
-        detectModel.outputs().putFloat(0);
-        wake.process(context, frameBuffer);
-
-        wake.process(context, frameBuffer);
-        wake.process(context, frameBuffer);
-        wake.process(context, frameBuffer);
-
-        assertEquals(SpeechContext.Event.DEACTIVATE, this.event);
-        assertFalse(context.isActive());
-    }
-
-    public void onEvent(SpeechContext.Event event, SpeechContext context) {
-        this.event = event;
     }
 
     public static class TestVad implements SpeechProcessor {
@@ -224,6 +421,77 @@ public class WakewordTriggerTest implements OnSpeechEventListener {
         public void run() {
             this.inputs().rewind();
             this.outputs().rewind();
+        }
+
+        public final void setOutputs(float ...outputs) {
+            this.outputs().rewind();
+            for (float o: outputs)
+                this.outputs().putFloat(o);
+        }
+    }
+
+    public class TestEnv implements OnSpeechEventListener {
+        public final TensorflowModel.Loader loader;
+        public final TestModel filter;
+        public final TestModel detect;
+        public final ByteBuffer frame;
+        public final TestVad vad;
+        public final WakewordTrigger wake;
+        public final SpeechContext context;
+        public SpeechContext.Event event;
+
+        public TestEnv(SpeechConfig config) {
+            // fetch configuration parameters
+            int sampleRate = config.getInteger("sample-rate");
+            int frameWidth = config.getInteger("frame-width");
+            int windowSize = config.getInteger("fft-window-size");
+            int fftSize = windowSize / 2 + 1;
+            int hopLength = config.getInteger("fft-hop-length");
+            int melLength = config.getInteger("mel-frame-length") * sampleRate / 1000 / hopLength;
+            int melWidth = config.getInteger("mel-frame-width");
+            String[] words = config.getString("wake-words").split(",");
+
+            // create/mock tensorflow-lite models
+            this.loader = spy(TensorflowModel.Loader.class);
+            this.filter = mock(TestModel.class);
+            this.detect = mock(TestModel.class);
+
+            doReturn(ByteBuffer
+                        .allocateDirect(fftSize * 4)
+                        .order(ByteOrder.nativeOrder()))
+                .when(this.filter).inputs();
+            doReturn(ByteBuffer
+                        .allocateDirect(melWidth * 4)
+                        .order(ByteOrder.nativeOrder()))
+                .when(this.filter).outputs();
+            doReturn(ByteBuffer
+                        .allocateDirect(melLength * melWidth * 4)
+                        .order(ByteOrder.nativeOrder()))
+                .when(this.detect).inputs();
+            doReturn(ByteBuffer
+                        .allocateDirect((words.length + 1) * 4)
+                        .order(ByteOrder.nativeOrder()))
+                .when(this.detect).outputs();
+            doCallRealMethod().when(this.filter).run();
+            doCallRealMethod().when(this.detect).run();
+            doReturn(this.filter).doReturn(this.detect).when(this.loader).load();
+
+            // create the frame buffer, vad mock, and wakeword trigger
+            this.frame = ByteBuffer.allocateDirect(frameWidth * sampleRate / 1000 * 2);
+            this.vad = spy(TestVad.class);
+            this.wake = new WakewordTrigger(config, this.loader, this.vad);
+
+            // create the speech context for processing calls
+            this.context = new SpeechContext();
+            context.addOnSpeechEventListener(this);
+        }
+
+        public void process() throws Exception {
+            this.wake.process(this.context, this.frame);
+        }
+
+        public void onEvent(SpeechContext.Event event, SpeechContext context) {
+            this.event = event;
         }
     }
 }
