dbe354abd126ee4216a699499f50384e867a6a99
Josh Ziegler <josh@pylon.com>
2020-02-05 12:36:12 -0500

    PR feedback
    This turns NLUService into a simple interface, representing
    the asynchronous nature of classification by returning a
    Future that the client can choose to immediately wait for
    or inspect later. This removes the need for event listeners
    and explicit event dispatch in NLU components.
    
    The redundant type field for slots has also been removed.

--- src/main/java/io/spokestack/spokestack/nlu/NLUService.java
+++ src/main/java/io/spokestack/spokestack/nlu/NLUService.java
@@ -1,52 +1,21 @@
 package io.spokestack.spokestack.nlu;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
 
 /**
- * A base class for components that provide intent classification and slot
+ * A simple interface for components that provide intent classification and slot
  * recognition, either on-device or via a network request.
- *
- * <p>
- * Intent classification is designed to be an asynchronous process; in order
- * to receive results as they are available, add a listener to the NLU
- * service.
- * </p>
  */
-public abstract class NLUService {
+public interface NLUService {
 
     /**
-     * Listeners that receive results from this component.
-     */
-    private List<NLUListener> listeners = new ArrayList<>();
-
-    /**
-     * Add an NLU listener to receive results from this component.
+     * Classifies a user utterance into an intent asynchronously.
      *
-     * @param listener The listener to add.
+     * @param utterance The user utterance to be classified.
+     * @param context   Any contextual information that should be sent along
+     *                  with the utterance to assist classification.
+     * @return A {@code Future} representing the completed classification task.
      */
-    public void addListener(NLUListener listener) {
-        this.listeners.add(listener);
-    }
-
-    /**
-     * Classifies a user utterance into an intent.
-     *
-     * Implementations should ensure that {@link #dispatch(NLUResult)} is
-     * called when a result is available so that listeners are notified.
-     *
-     * @param request The user request to be classified.
-     */
-    public abstract void classify(NLURequest request);
-
-    /**
-     * Dispatch an NLU result to all registered listeners.
-     *
-     * @param result The result to dispatch.
-     */
-    public void dispatch(NLUResult result) {
-        for (NLUListener listener : listeners) {
-            listener.resultReceived(result);
-        }
-    }
+    Future<NLUResult> classify(String utterance, Map<String, Object> context);
 }
--- src/main/java/io/spokestack/spokestack/nlu/Slot.java
+++ src/main/java/io/spokestack/spokestack/nlu/Slot.java
@@ -5,33 +5,22 @@ package io.spokestack.spokestack.nlu;
  *
  * <p>
  * Depending on the NLU service used, slots may be typed; if present, the type
- * of each slot can be used to cast its raw value to the appropriate class
- * after NLU has finished.
+ * of each slot can be accessed by calling {@code getClass()} on the slot's
+ * value.
  * </p>
  */
 public final class Slot {
     private final String slotName;
-    private final Class<?> slotType;
     private final Object slotValue;
 
     /**
-     * Create a new untyped slot.
-     * @param name The slot's name.
+     * Create a new slot.
+     *
+     * @param name  The slot's name.
      * @param value The slot's value.
      */
     public Slot(String name, Object value) {
-        this(name, null, value);
-    }
-
-    /**
-     * Create a new typed slot.
-     * @param name The slot's name.
-     * @param type The slot's type.
-     * @param value The slot's value.
-     */
-    public Slot(String name, Class<?> type, Object value) {
         this.slotName = name;
-        this.slotType = type;
         this.slotValue = value;
     }
 
@@ -40,13 +29,6 @@ public final class Slot {
      */
     public String getName() {
         return slotName;
-    }
-
-    /**
-     * @return The slot's type.
-     */
-    public Class<?> getType() {
-        return slotType;
     }
 
     /**
