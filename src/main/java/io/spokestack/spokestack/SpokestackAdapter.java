a440c845b165a1cad1f274bb579cf15689c1fa13
Josh Ziegler <josh@pylon.com>
2020-09-18 15:08:24 -0400

    Update SpokestackAdapter method doc for clarity

--- src/main/java/io/spokestack/spokestack/SpokestackAdapter.java
+++ src/main/java/io/spokestack/spokestack/SpokestackAdapter.java
@@ -23,26 +23,59 @@ public abstract class SpokestackAdapter implements
       TraceListener,
       TTSListener {
 
+    /**
+     * Receive events from the speech pipeline.
+     *
+     * @param event   The name of the event that was raised.
+     * @param context The current speech context.
+     */
     @Override
     public void onEvent(@NotNull SpeechContext.Event event,
-                        @NotNull SpeechContext context) throws Exception {
+                        @NotNull SpeechContext context) {
     }
 
+    /**
+     * Receive trace messages from the NLU subsystem.
+     *
+     * @param level   The trace event's severity level.
+     * @param message the trace event's message.
+     */
     @Override
     public void onTrace(@NotNull EventTracer.Level level,
                         @NotNull String message) {
     }
 
+    /**
+     * Receive events from the TTS subsystem.
+     *
+     * @param event The event from the TTS subsystem.
+     */
     @Override
     public void eventReceived(@NotNull TTSEvent event) {
 
     }
 
+    /**
+     * Called when an NLU classification result is available if this class is
+     * registered as a callback at classification time.
+     *
+     * @param result The NLU result.
+     * @see io.spokestack.spokestack.nlu.tensorflow.TensorflowNLU#classify(String)
+     * @see io.spokestack.spokestack.util.AsyncResult
+     */
     @Override
-    public void call(@NotNull NLUResult arg) {
+    public void call(@NotNull NLUResult result) {
 
     }
 
+    /**
+     * Receive notifications of errors that occur during NLU classification if
+     * this class is registered as a callback at classification time.
+     *
+     * @param err An error generated during expected NLU classification.
+     * @see io.spokestack.spokestack.nlu.tensorflow.TensorflowNLU#classify(String)
+     * @see io.spokestack.spokestack.util.AsyncResult
+     */
     @Override
     public void onError(@NotNull Throwable err) {
 
