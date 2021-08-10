f1175586b73dd21a0b5a3d357f2581f9616b232b
Josh Ziegler <josh@pylon.com>
2021-08-10 16:38:09 -0400

    fix: avoid timeout for empty Azure transcripts

--- src/main/java/io/spokestack/spokestack/microsoft/AzureSpeechRecognizer.java
+++ src/main/java/io/spokestack/spokestack/microsoft/AzureSpeechRecognizer.java
@@ -231,16 +231,13 @@ public class AzureSpeechRecognizer implements SpeechProcessor {
               SpeechRecognitionEventArgs recognitionArgs) {
             ResultReason reason = recognitionArgs.getResult().getReason();
             String transcript = recognitionArgs.getResult().getText();
-            boolean isFinal = (reason == ResultReason.RecognizedSpeech);
             if (!transcript.equals("")) {
-                if (isFinal) {
+                if (reason == ResultReason.RecognizedSpeech) {
                     dispatchResult(transcript, SpeechContext.Event.RECOGNIZE);
                 } else if (reason == ResultReason.RecognizingSpeech) {
                     dispatchResult(transcript,
                           SpeechContext.Event.PARTIAL_RECOGNIZE);
                 }
-            } else if (isFinal) {
-                this.speechContext.dispatch(SpeechContext.Event.TIMEOUT);
             }
         }
 
